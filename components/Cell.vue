c757a360b5f7fbd64d9378ed739549d3627a5851
Ilya Smelkov <ismelkov@restream.rt.ru>
2018-01-09 15:47:53 +0300

    Dynamic cells 🈸

---
+++ components/Cell.vue
@@ -0,0 +1,82 @@
+<template lang='slm'>
+  td.corner :class='image'
+    span.player v-for='player in playersIds' :class='"id" + player'
+</template>
+
+<script>
+
+export default {
+  name: 'Cell',
+  props: ['i', 'j', 'players', 'image'],
+  computed: {
+    playersIds() {
+      let ids = []
+
+      this.players.forEach((player, index) => {
+        if (this.i == player.i && this.j == player.j) {
+          ids.push(index)
+        }
+      })
+
+      return ids
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+td {
+  border: 1px solid black;
+  width: 50px;
+  height: 50px;
+
+  &.corner {
+    background-position: center center;
+    background-size: 100% 100%;
+  }
+
+  &.parking {
+    background-image: url(/parking.png);
+  }
+
+  &.police {
+    background-image: url(/police.svg);
+  }
+
+  &.jail {
+    background-image: url(/jail.png);
+  }
+
+  &.salary {
+    background-image: url(/salary.png);
+  }
+
+  img {
+    max-width: 100%;
+    max-height: 100%;
+  }
+}
+
+.player {
+  width: 20px;
+  height: 20px;
+  border-radius: 10px;
+  display: inline-block;
+
+  &.id0 {
+    background-color: red;
+  }
+
+  &.id1 {
+    background-color: green;
+  }
+
+  &.id2 {
+    background-color: blue;
+  }
+
+  &.id3 {
+    background-color: purple;
+  }
+}
+</style>
--- components/Game.vue
+++ components/Game.vue
@@ -1,52 +1,20 @@
 <template lang="slm">
   div
     table
-      tr
-        td
-          img src='parking.png'
-        td
-        td
-        td
-        td
-        td
-        td
-        td
-        td
-          img src='police.svg'
-      tr
-        td
-        td colspan=7 rowspan=7
-        td
-      tr
-        td
-        td
-      tr
-        td
-        td
-      tr
-        td
-        td
-      tr
-        td
-        td
-      tr
-        td
-        td
-      tr
-        td
-        td
-      tr
-        td
-          img src='jail.png'
-        td
-        td
-        td
-        td
-        td
-        td
-        td
-        td
-          img src='salary.png'
+      tr v-for='(row, i) in cells'
+        template v-for='(cell, j) in row'
+          DeskCell[
+            v-if='cell'
+            :i='i'
+            :j='j'
+            :players='players'
+            :image='cell.image'
+          ]
+          td[
+            rowspan=7
+            colspan=7
+            v-else-if='i == 1 && j == 1'
+          ]
     h1
       Dice> v-for='(dice, i) in dices' :value='dice' :key="i"
       button @click="roll"
@@ -55,6 +23,7 @@
 
 <script>
 import Dice from './Dice'
+import DeskCell from './Cell'
 
 const randomInteger = function (min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min))
@@ -62,14 +31,46 @@ const randomInteger = function (min, max) {
 
 const randDice = function() { return randomInteger(1, 6) }
 
+class Cell {
+  constructor(moveDir, image) {
+    this.moveDir = moveDir
+    this.image = image
+  }
+}
+
+class Player {
+  constructor() {
+    this.i = 8
+    this.j = 8
+  }
+}
+
+const parkingCell = new Cell('right', 'parking')
+const jailCell = new Cell('up', 'jail')
+const policeCell = new Cell('down', 'police')
+const salaryCell = new Cell('left', 'salary')
+
+let cells = new Array(9);
+for (let i = 0; i < 9; i++) {
+  cells[i] = new Array(9);
+}
+
+cells[0][0] = parkingCell
+cells[0][8] = policeCell
+cells[8][0] = jailCell
+cells[8][8] = salaryCell
+
 export default {
   name: 'Game',
   data() {
     return {
-      dices: [randDice(), randDice()]
+      dices: [randDice(), randDice()],
+      cells: cells,
+      players: [new Player(), new Player(), new Player(), new Player()]
     }
   },
   components: {
+    DeskCell,
     Dice
   },
   methods: {
@@ -80,15 +81,5 @@ export default {
 }
 </script>
 
-<style>
-td {
-  border: 1px solid black;
-  width: 50px;
-  height: 50px;
-}
-
-td img {
-  max-width: 100%;
-  max-height: 100%;
-}
+<style lang="scss">
 </style>
